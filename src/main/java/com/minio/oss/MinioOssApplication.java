713a86b4c3f219632d34e6d3217669130e2922bb
lihao <lihao3@foxmail.com>
2022-08-16 15:10:36 +0800

    优化

--- src/main/java/com/minio/oss/MinioOssApplication.java
+++ src/main/java/com/minio/oss/MinioOssApplication.java
@@ -3,12 +3,8 @@ package com.minio.oss;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @MapperScan("com.minio.oss.mapper")
-@RefreshScope
-@EnableDiscoveryClient
 @SpringBootApplication
 public class MinioOssApplication {
 
